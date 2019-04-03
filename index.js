8f5cab7fe31914b29c6dbbca3add71d303887442
kevin.balicot <kevin.balicot@pmsipilot.com>
2019-04-03 11:09:27 +0200

    feat: Update for megaquery-js 1.9.0 | :zap:

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { SocketServer } = require('megaquery-js/server');
 const createMegaqueryServer = (server, app, options, callback) => {
     const socketServer = new SocketServer(Object.assign({ server }, options), callback);
 
-    socketServer.on('dbconnected', (requester) => app.requester = requester);
+    app.requester = socketServer.requester;
 
     return socketServer;
 }
